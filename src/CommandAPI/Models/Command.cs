cefc9d0e014bd69a62b91f3eaef0da4a3fb66595
Jason Huish <jmhuish02@gmail.com>
2021-12-02 15:29:59 -0600

    Added Command Model to API Project

---
+++ src/CommandAPI/Models/Command.cs
@@ -0,0 +1,6 @@
+public class Command {
+    public int Id { get; set; }
+    public string HowTo { get; set; }
+    public string Platform { get; set; }
+    public string  CommandLine { get; set; }
+}
